aaaf5e89a88ea8081f39ab1a8e6fa39ab6ef8579
sanjeevkuinkel <sanjeeevkuinkel1111@gmail.com>
2025-01-22 16:13:31 +0545

    add few blog api

--- routes/blog.route.js
+++ routes/blog.route.js
@@ -1,10 +1,11 @@
 import express from "express";
 import { isAuthor } from "../middleware/auth.middleware.js";
-import { createBlog } from "../services/blog.service.js";
+import { createBlog, getAuthorBlogs, getBlogs, getSingleBlog, updateBlog } from "../services/blog.service.js";
 const router = express.Router();
 router.post("/blog/create", isAuthor,createBlog);
-router.post("/blog/details", (req, res) => {});
-router.post("/blogs", (req, res) => {});
-router.post("/blog/update/:id", (req, res) => {});
-router.post("/blog/delete/:id", (req, res) => {});
+router.get("/blog/:id",getSingleBlog);
+router.get("/blogs", getBlogs);
+router.post("/blogs/:id",isAuthor,getAuthorBlogs)
+router.put("/blog/update/:id",updateBlog);
+router.delete("/blog/delete/:id", (req, res) => {});
 export { router };
--- services/blog.service.js
+++ services/blog.service.js
@@ -1,4 +1,4 @@
-import { blogPostValidationSchema } from "../config/blog.validation.js";
+import { checkMongoIdValidation } from "../config/mongoIdValidator.js";
 import {Blog} from "../models/blog.model.js";
 
 export const createBlog = async (req, res) => {
@@ -28,3 +28,45 @@ export const createBlog = async (req, res) => {
         return res.status(400).send({ message: "Bad Request", details: error.message });
     }
 };
+export const getSingleBlog=async (req, res) => {
+    
+      try {
+        const blogId=req.params.id;
+    const  isValidMongoId=checkMongoIdValidation(blogId);
+    if (!isValidMongoId) {
+        return res.status(400).send({ message: "Invalid Mongodb Id." });
+      }
+      const blog=await Blog.findById(blogId);
+      if(!blog)
+        return res.status(404).send({message:"Blog Doesnot Exist."})
+      return res.status(200).send({blog})
+      } catch (error) {
+        console.log("Error occured",error.message)
+      }
+};
+export const getBlogs=async(req,res)=>{
+   try {
+    const blogs=await Blog.find()
+    if(!blogs)return res.status(404).send({message:"Blog Doesnot Exist."})
+        return res.status(200).send({blogs})
+
+   } catch (error) {
+    console.error("Error fetching blogs:", error.message);
+    return res.status(500).send({ message: "Internal server error."})
+   }
+};
+export const getAuthorBlogs=async(req,res)=>{
+    const authorId=req.params.id;
+    console.log(authorId)
+    const isValidMongoId=checkMongoIdValidation(authorId);
+    if (!isValidMongoId) {
+        return res.status(400).send({ message: "Invalid Mongodb Id." });
+      }
+    const blogs=await Blog.find({author:authorId})
+    console.log(blogs)
+    
+
+}
+export const updateBlog= (req, res) => {
+
+}
